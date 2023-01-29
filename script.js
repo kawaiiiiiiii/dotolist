8021e2f97b434c3e2b45523c025418c56a232a5f
DESKTOP-62NBI5U\yshkw <yshkwi.chacha@gmail.com>
2023-01-30 08:07:32 +0900

    cssトップダウン一旦保留

--- script.js
+++ script.js
@@ -1,98 +1,212 @@
-// Localstorageの活用を含めたコード設計説明
-// 16.配列をつくる
-// let data = ["食う", "寝る", "遊ぶ"]
-//            ↓この配列がJそんに
-// 22.保存されたものをリロードじ取り出し、④を行う
-let data = JSON.parse(localStorage.getItem("key"));;
-// 17.コンソールに配列の中身を出す
-if (data) { // 23.配列の中身が空だった時の条件を付け加える
-    for (const d of data) {
-        console.log(d);
-        // 19.関数使ってリロード時に配列の中身を出すようにする。
-        taskList(d)
-    }
-} else {
-    data = [];
-}
+//メモの選択ボタン
+// htmlボタン--------------------------------------------
+// 1　ボタン要素を取得
+let memoBtn = document.querySelectorAll("#memoBtn");
+// console.log(memoBtn);
+// console.log(memoBtn[0].value);///////////
+// // 2 ボタンにクリックイベントを追加
+memoBtn[0].addEventListener("click",function () {
+    // input要素を作成 
+    let memoInputTitle = document.createElement("input");
+    let memoInputContent = document.createElement("textarea");
+    let memoInputLink = document.createElement("input");
+    let memoInputBtn = document.createElement("input");
+    // console.log(memoInputLink);
 
+    // タグ名を入力
+    memoInputTitle.setAttribute("type","text");
+    memoInputTitle.setAttribute("id","title");
+    memoInputTitle.setAttribute("class","input");
+    memoInputTitle.setAttribute("placeholder","タグ名を入力");
+    
+    // 詳細を入力
+    memoInputContent.setAttribute("name","content");
+    memoInputContent.setAttribute("id","Content");
+    memoInputContent.setAttribute("class","input");
+    memoInputContent.setAttribute("placeholder","詳細を入力");
+    memoInputContent.setAttribute("cols","30");
+    memoInputContent.setAttribute("rows","10");
 
-// 1 ボタン要素を取得(変数名btn)
-let btn = document.querySelector("#btn")
-// console.log(btn);
+    // リンクを入力
+    memoInputLink.setAttribute("type","text");
+    memoInputLink.setAttribute("id","Link");
+    memoInputLink.setAttribute("class","input");
+    memoInputLink.setAttribute("placeholder","参考ページのリンク");
 
-// 2 ボタンにクリックイベントを追加する (3-16は全てこのイベント内)
-btn.addEventListener("click", function () {
-    // 3 ユーザーが入力した要素を取得 (変数名input)
-    let input = document.querySelector("#input");
-    // console.log(input);
-    // 4 上で取得した要素の入力された内容を取得し、変数(task)に代入
-    let task = input.value;
-    // console.log(task);
-    // やることがインプットされた時のみ以下処理がされるぅ
-    if (task !== "") {
-        taskList(task);
-        // 20.その後追加されたタスクを配列にPUSHする
-        data.push(task)
-        // 21.配列をローカルストレージに保存する
-        let dataText = JSON.stringify(data);
-        // 21.5 保存するにはJSON化で文字列に！
-        localStorage.setItem("key", dataText);
-    }
+    // 記憶より記憶ボタン
+    memoInputBtn.setAttribute("type","button");
+    memoInputBtn.setAttribute("id","kiroku");
+    memoInputBtn.setAttribute("value","記憶より記録");
+
+    // input要素を親にあわす
+    let memoInputP = document.querySelector(".memo-Input")
+    // console.log(memoInputP);
+    memoInputP.appendChild(memoInputTitle);
+    memoInputP.appendChild(memoInputContent);
+    memoInputP.appendChild(memoInputLink);
+    memoInputP.appendChild(memoInputBtn);
+
+// 記憶より記録
+let kirokuBtn = document.querySelector("#kiroku");
+// console.log(kirokuBtn);
+// 記憶より記録ボタンにイベントを追加
+kirokuBtn.addEventListener("click",function () {
+    // ユーザーが入力した内容を取得
+    let input = document.querySelectorAll(".input");
+    kiroku()
 })
+})
+// htmlボタン終了
 
-// // Localstorageの活用を含めたコード設計説明
+// cssボタン--------------------------------------------
+// 1　ボタン要素を取得
+// let memoBtn = document.querySelectorAll("#memoBtn");
+// console.log(memoBtn);
+// console.log(memoBtn[0].value);///////////
+// // 2 ボタンにクリックイベントを追加
+memoBtn[1].addEventListener("click",function () {
+    // input要素を作成 
+    let memoInputTitle = document.createElement("input");
+    let memoInputContent = document.createElement("textarea");
+    let memoInputLink = document.createElement("input");
+    let memoInputBtn = document.createElement("input");
+    let momeInputSelect = document.createElement("select")
+    let momeInputOption1 = document.createElement("option")
+    let momeInputOption2 = document.createElement("option")
+    let momeInputOption3 = document.createElement("option")
+    console.log(momeInputOption2);
 
-// ---------------------------------------------
-// まず読み込んだら前回保存したデータを読み込んで欲しいからこの部分をいちばん上に持っていく
-// // 16.配列をつくる
-// let data = ["食う","寝る","遊ぶ"]
-// // 17.コンソールに配列の中身を出す
-// for (const d of data) {
-//     console.log(d);
+    // タグ名を入力
+    memoInputTitle.setAttribute("type","text");
+    memoInputTitle.setAttribute("id","title");
+    memoInputTitle.setAttribute("class","input");
+    memoInputTitle.setAttribute("placeholder","プロパティー名を入力");
+    
+    // 詳細を入力
+    memoInputContent.setAttribute("name","content");
+    memoInputContent.setAttribute("id","Content");
+    memoInputContent.setAttribute("class","input");
+    memoInputContent.setAttribute("placeholder","詳細を入力");
+    memoInputContent.setAttribute("cols","30");
+    memoInputContent.setAttribute("rows","10");
+
+    // リンクを入力
+    memoInputLink.setAttribute("type","text");
+    memoInputLink.setAttribute("id","Link");
+    memoInputLink.setAttribute("class","input");
+    memoInputLink.setAttribute("placeholder","参考ページのリンク");
+
+    // どんな装飾？一旦保留
+//     momeInputOption1.text = "配置"
+//     momeInputOption2.text = "サイズ"
+//     momeInputOption3.text = "見た目"
+//    console.log(momeInputSelect.appendChild(momeInputOption[i])); momeInputSelect.appendChild(momeInputOption1)
+//     // for (let i = 1; i <= 3; i++) {
+//     //    console.log(momeInputSelect.appendChild(momeInputOption[i]));  
+//     // }
+    
+
+    // 記憶より記憶ボタン
+    memoInputBtn.setAttribute("type","button");
+    memoInputBtn.setAttribute("id","kiroku");
+    memoInputBtn.setAttribute("value","記憶より記録");
+
+    // input要素を親にあわす
+    let memoInputP = document.querySelector(".memo-Input")
+    // console.log(memoInputP);
+    memoInputP.appendChild(memoInputTitle);
+    memoInputP.appendChild(memoInputContent);
+    memoInputP.appendChild(memoInputLink);
+    memoInputP.appendChild(memoInputBtn);
+    // memoInputP.appendChild(momeInputSelect);
+
+// 記憶より記録
+let kirokuBtn = document.querySelector("#kiroku");
+// console.log(kirokuBtn);
+// 記憶より記録ボタンにイベントを追加
+kirokuBtn.addEventListener("click",function () {
+    // ユーザーが入力した内容を取得
+    let input = document.querySelectorAll(".input");
+    kiroku()
+})
+})
+// cssボタン終了
+
+
+// function MEMOBTN() {
+//         // input要素を作成
+//         let memoInputTitle = document.createElement("input");
+//         let memoInputContent = document.createElement("textarea");
+//         let memoInputLink = document.createElement("input");
+//         let memoInputBtn = document.createElement("input");
+//         // console.log(memoInputLink);
+//         // タグ名を入力
+//         memoInputTitle.setAttribute("type","text");
+//         memoInputTitle.setAttribute("id","title");
+//         memoInputTitle.setAttribute("class","input");
+//         memoInputTitle.setAttribute("placeholder","タグ名を入力");
+        
+//         // 詳細を入力
+//         memoInputContent.setAttribute("name","content");
+//         memoInputContent.setAttribute("id","Content");
+//         memoInputContent.setAttribute("class","input");
+//         memoInputContent.setAttribute("placeholder","タグの意味を入力");
+//         memoInputContent.setAttribute("cols","30");
+//         memoInputContent.setAttribute("rows","10");
+    
+//         // リンクを入力
+//         memoInputLink.setAttribute("type","text");
+//         memoInputLink.setAttribute("id","Link");
+//         memoInputLink.setAttribute("class","input");
+//         memoInputLink.setAttribute("placeholder","参考ページのリンク");
+    
+//         // 記憶より記憶ボタン
+//         memoInputBtn.setAttribute("type","button");
+//         memoInputBtn.setAttribute("id","kiroku");
+//         memoInputBtn.setAttribute("value","記憶より記録");
+    
+//         // input要素を親にあわす
+//         let memoInputP = document.querySelector(".memo-Input")
+//         // console.log(memoInputP);
+//         memoInputP.appendChild(memoInputTitle);
+//         memoInputP.appendChild(memoInputContent);
+//         memoInputP.appendChild(memoInputLink);
+//         memoInputP.appendChild(memoInputBtn);
 // }
-// ----------------------------------------------
+// 
 
-// 18.関数を作る（関数にしただけで問題なく動くか確認）
-// ↑どの番号からどの番号までが同じ処理になるの？4番から15番まで
-// 18.5 引数無しの関数を一回実施、その後引数あり関数に変更
-function taskList(task) {
-    // 5 <li>要素を作成 (変数名list)
-    let list = document.createElement("li");
+
+function kiroku() {// 記憶より記録ボタン押したら下に出力してくれる内容の
+    let input = document.querySelectorAll(".input");
+    // console.log(input);
+    // 上で取得した要素の入力された内容を取得し,変数に代入yeah!
+    // タイトル
+    let title = input[0].value;
+    console.log(title);
+    // // 内容
+    let content = input[1].value;
+    console.log(content);
+    // // リンク
+    let link = input[2].value;
+    console.log(link);
+
+    // li要素を作成
+    let list_title = document.createElement("dt");
+    let list_content = document.createElement("dd");
+    let list_link = document.createElement("dd");
     // console.log(list);
-    // 6 <li>にクラス（list）を追加
-    list.classList.add("list")
-    // 7 <li>にユーザーが入力した内容を入れる
-    list.textContent = task
-    // 8 <ul>要素を取得(変数名list_wrap)
-    let list_wrap = document.querySelector(".todo-list");
-    // console.log(list_wrap);
-    // 9 4-6で作成した<li>(list)を、8で取得した<ul>(listWrap)の子要素に入れる
-    list_wrap.appendChild(list);
-    // 10 inputの中に入力された文字を消去する
-    input.value = ""
-    // 11 Deleteボタンを作成（機能としてはボタンですが、<div>要素を使います）(変数名deleteBtn)
-    let deleteBtn = document.createElement("div");
-    // console.log(deleteBtn);
-    // 12 Deleteボタンのコンテンツ (文字) を「delete」にする
-    deleteBtn.textContent = "delete";
-    // 13 Deleteボタンにクラス (delete) を追加する
-    deleteBtn.classList.add("delete");
-    // 14 11-13で作成したDeleteボタン(deleteBtn)を、5-8で作成した<li>(list)の子要素に入れる
-    list.appendChild(deleteBtn);
-    // 15 Deleteボタンにクリックイベントを追加 
-    deleteBtn.addEventListener("click", function () {
-        // 16 クリックした要素 (this) を親ごと削除
-        deleteBtn.parentElement.remove()
-        // 順番を特定するための関数indexOf();
-        let target = data.indexOf(task);
-        // 24.削除した時に配列からも削除するようにする
-        data.splice(target, 1);
-        // 25.削除後の内容をローカルストレージに保存
-        let dataText = JSON.stringify(data);
-        localStorage.setItem("key", dataText);
-    })
+    // liにクラス名を追加
+    list_title.classList.add("list-title")
+    // liにユーザーが入力した内容を入れる
+    list_title.textContent = title;
+    list_content.textContent = content;
+    list_link.textContent = link;
 
+    // ulの要素を取得
+    let list_wrap = document.querySelector(".memo-list");
+    console.log(list_wrap);
+    // ul(親)にli(子)を会わせてあげる
+    list_wrap.appendChild(list_title);
+    list_wrap.appendChild(list_content);
+    list_wrap.appendChild(list_link);
 }
-
-
-
